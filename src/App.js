0e8005b69674fdf540bc33a8473e1ec5cc1dda45
Ryan <rygho90@gmail.com>
2022-08-02 07:15:38 -0400

    Add HTML for general info input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <form>
+          <label htmlFor="firstNameInput">First Name</label>
+          <input type="text" id="firstNameInput"></input>
+          <label htmlFor="lastNameInput">Last Name</label>
+          <input type="text" id="lastNameInput"></input>
+          <label htmlFor="emailInput">Email Address</label>
+          <input type="text" id="emailInput"></input>
+          <label htmlFor="phoneInput">Phone Number</label>
+          <input type="text" id="phoneInput"></input>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
