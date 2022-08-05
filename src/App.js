6167ebf9294e763c4a61a20aacb962a8a408a9b5
Ryan <rygho90@gmail.com>
2022-08-05 12:34:52 -0400

    Change App to class component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div class="main-container">
-      <div class="sidebar">
-        <div class="sidebar-box">
-          <div class="sidebar-item selected-item">Home</div>
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div class="main-container">
+        <div class="sidebar">
+          <div class="sidebar-box">
+            <div class="sidebar-item selected-item">Home</div>
+          </div>
+          <div class="sidebar-box">
+            <div class="sidebar-item">General Information</div>
+          </div>
+          <div class="sidebar-box">
+            <div class="sidebar-item">Education</div>
+          </div>
+          <div class="sidebar-box">
+            <div class="sidebar-item">Work Experience</div>
+          </div>
+          <div class="sidebar-box">
+            <div class="sidebar-item">Skills</div>
+          </div>
         </div>
-        <div class="sidebar-box">
-          <div class="sidebar-item">General Information</div>
-        </div>
-        <div class="sidebar-box">
-          <div class="sidebar-item">Education</div>
-        </div>
-        <div class="sidebar-box">
-          <div class="sidebar-item">Work Experience</div>
-        </div>
-        <div class="sidebar-box">
-          <div class="sidebar-item">Skills</div>
+
+        <div class="content-container">
+          <h1>Lorem ipsum dolor sit amet.</h1>
+
+          <div>
+            <form onSubmit={this.onSubmitGeneral}>
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                id="firstNameInput"
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
         </div>
       </div>
-      <div class="content-container">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
