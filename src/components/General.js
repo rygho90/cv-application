4235e421cfa35fd940f3a518861ab04619805b9e
Ryan <rygho90@gmail.com>
2022-08-07 20:00:39 -0400

    Add HTML for general info screen

--- src/components/General.js
+++ src/components/General.js
@@ -7,14 +7,25 @@ export default class General extends Component {
   }
 
   handleSwitch() {
-    this.props.onSwitch('Home')
+    this.props.onSwitch("Home");
   }
 
   render() {
-    if (this.props.visible !== 'General') return null;
+    if (this.props.visible !== "General") return null;
     return (
       <div>
         <div>General Information</div>
+        <form className="general-form">
+          <label for="fname">First Name:</label><br />
+          <input type="text" id="fname"></input><br />
+          <label for="lname">Last Name:</label><br />
+          <input type="text" id="lname"></input><br />
+          <label for="email">Email:</label><br />
+          <input type="text" id="email"></input><br />
+          <label for="phone">Phone Number:</label><br />
+          <input type="text" id="phone"></input><br />
+          <button>Save and Continue</button>
+        </form>
         <button onClick={this.handleSwitch}>Home</button>
       </div>
     );
